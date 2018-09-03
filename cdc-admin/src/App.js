6a375432464390c762f2e47a91c883fa242e7857
Geison Santos <geison.sn@gmail.com>
2018-09-03 02:08:32 -0400

    Configura o atributo key para otimizar a atualização do componente

--- cdc-admin/src/App.js
+++ cdc-admin/src/App.js
@@ -78,7 +78,7 @@ class App extends Component {
                     {
                       this.state.lista.map(autor => {
                         return (
-                          <tr>
+                          <tr key={autor.id}>
                             <td>{autor.nome}</td>
                             <td>{autor.email}</td>
                           </tr>
