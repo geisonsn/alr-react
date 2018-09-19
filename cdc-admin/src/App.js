505893309c58ef1b0c6a51dbcc4dfb351685bf31
Geison Santos <geison.sn@gmail.com>
2018-09-19 14:31:17 -0400

    Componentiza o botão de gravar autor

--- cdc-admin/src/App.js
+++ cdc-admin/src/App.js
@@ -3,6 +3,7 @@ import './css/pure-min.css';
 import './css/side-menu.css';
 import $ from 'jquery';
 import InputCustomizado from './componentes/InputCustomizado';
+import BotaoSubmitCustomizado from './componentes/BotaoSubmitCustomizado';
 
 class App extends Component {
   constructor() {
@@ -116,10 +117,7 @@ class App extends Component {
                       onChange={this.setSenha}
                   />
 
-                  <div className="pure-control-group">                                  
-                    <label></label> 
-                    <button type="submit" className="pure-button pure-button-primary">Gravar</button>                                    
-                  </div>
+                  <BotaoSubmitCustomizado label="Gravar"/>
                 </form>             
 
               </div>  
