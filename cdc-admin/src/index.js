4b78eb4dbb176b05e4cb70563ef752bc0db38416
Geison Santos <geison.sn@gmail.com>
2018-10-11 20:03:16 -0400

    Instala versão 4 do React Router

--- cdc-admin/src/index.js
+++ cdc-admin/src/index.js
@@ -4,16 +4,18 @@ import './index.css';
 import App from './App';
 import AutorBox from './Autor';
 import Home from './Home';
-import { Router, Route, browserHistory, IndexRoute } from 'react-router';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-    (<Router history={browserHistory}>
-            <Route path="/" component={App} >
-                <IndexRoute component={Home}/>
-                <Route path="/autor" component={AutorBox}/>
-                <Route path="/livro" />
-            </Route>
+    (<Router>
+        <App>
+                <Switch>            
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/autor" component={AutorBox}/>
+                    <Route path="/livro"/>
+                </Switch>            
+        </App>
     </Router>), 
     document.getElementById('root')
 );
